364d2aad1597dc590dc9384b48b59df129407d56
Ananth Bhaskararaman <antsub@gmail.com>
2022-10-31 00:19:08 +0530

    Start on some tests

--- src/VCard.elm
+++ src/VCard.elm
@@ -1,7 +1,14 @@
-module VCard exposing (VCard)
+module VCard exposing (VCard, toString)
 
-import VCard.V4 exposing (V4)
+import VCard.V4 exposing (V4, v4)
 
 
 type VCard
     = V4 V4
+
+
+toString : VCard -> String
+toString vc =
+    case vc of
+        V4 v ->
+            v4 v
